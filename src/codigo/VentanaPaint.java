cd2e2b1458111682701ce5f07497de4fb04b1c31
xp <xp@CETYSDAM015>
2018-02-21 15:35:57 +0100

    BUG:(solved)

--- src/codigo/VentanaPaint.java
+++ src/codigo/VentanaPaint.java
@@ -15,6 +15,7 @@ import java.io.File;
 import java.io.IOException;
 import javax.imageio.ImageIO;
 import javax.swing.JFileChooser;
+import javax.swing.JMenu;
 import javax.swing.JToggleButton;
 
 /**
@@ -246,6 +247,11 @@ public class VentanaPaint extends javax.swing.JFrame {
         });
 
         jMenu1.setText("File");
+        jMenu1.addChangeListener(new javax.swing.event.ChangeListener() {
+            public void stateChanged(javax.swing.event.ChangeEvent evt) {
+                jMenu1StateChanged(evt);
+            }
+        });
 
         jMenuItem1.setText("Guardar");
         jMenuItem1.addMouseListener(new java.awt.event.MouseAdapter() {
@@ -405,6 +411,13 @@ public class VentanaPaint extends javax.swing.JFrame {
             }
         }
     }//GEN-LAST:event_jMenuItem1MousePressed
+
+    private void jMenu1StateChanged(javax.swing.event.ChangeEvent evt) {//GEN-FIRST:event_jMenu1StateChanged
+        JMenu menu = (JMenu) evt.getSource();
+        if (!menu.isSelected()){
+            repaint();
+        }
+    }//GEN-LAST:event_jMenu1StateChanged
 
     /**
      * @param args the command line arguments
